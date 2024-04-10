564245700a01dc97b5ca2fbdf3a9a045a0c17577
Ahad Chowdhury <ahad@maacpiash.com>
2024-04-10 20:41:59 +1000

    ✨ Add Todo records and CRUD endpoints for them

--- server/Program.cs
+++ server/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var todos = new List<Todo>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,29 +18,46 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/todos", () => todos);
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/todos/{id:guid}", (Guid id) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    var todo = todos.Find(todo => todo.Id == id);
+    if (todo is null) return Results.NotFound();
+    return Results.Ok(todo);
+});
+
+app.MapPost("/todos", (TodoDTO dto) =>
+{
+    var (Id, Title, Done) = dto;
+    if (Id is not null)
+    {
+        var existing = todos.Find(todo => todo.Id == Id);
+        if (existing is not null)
+            return Results.Conflict("A todo with this Id already exists!");
+    }
+    var todo = new Todo(Id ?? Guid.NewGuid(), Title, Done);
+    todos.Add(todo);
+    return Results.Created($"/todos/{todo.Id}", todo);
+});
+
+app.MapPut("/todos/{id:guid}", (Guid id, TodoDTO dto) =>
+{
+    var (Id, Title, Done) = dto;
+    var index = todos.FindIndex(todo => todo.Id == id);
+    if (index is -1) return Results.NotFound();
+    todos[index] = todos[index] with { Title = Title, Done = Done };
+    return Results.Ok();
+});
+
+app.MapDelete("/todos/{id:guid}", (Guid id) =>
+{
+    var removed = todos.RemoveAll(todo => todo.Id == id);
+    if (removed is not 0) return Results.NoContent();
+    return Results.NotFound();
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+record Todo(Guid Id, string Title, bool Done);
+record TodoDTO(Guid? Id, string Title, bool Done);
