2dc621af0c9d1d5e3dbbf82a46e5975412ab44d2
Ahad Chowdhury <ahad@maacpiash.com>
2024-04-16 23:36:24 +1000

    ✨ Add styles to the whole page
    Also, a little semantic change in the HTML file 😉'

--- client/src/routes/todos/+page.svelte
+++ client/src/routes/todos/+page.svelte
@@ -3,20 +3,24 @@
     export let data
 </script>
 
-<form method="POST" action="?/create" use:enhance>
-    <label>
-        Task title
-        <input name="title" type="text" placeholder="Title" />
-    </label>
-    <label for="done">Done</label>
-    <input id="done" name="done" type="checkbox" role="switch" placeholder="Done" />
-    <input type="submit" value="Submit" />
-</form>
+<article class="flex flex-col md:flex-row justify-center gap-8">
+    <form method="POST" action="?/create" use:enhance class="flex flex-col justify-center gap-4">
+        <label>
+            Task title
+            <input name="title" type="text" placeholder="Title" class="input input-bordered w-full max-w-s" />
+        </label>
+        <div class="flex flex-row justify-start max-w-xs gap-4">
+            <label for="done"> Done </label>
+            <input id="done" name="done" type="checkbox" class="toggle" />
+        </div>
+        <button class="btn btn-primary">Submit</button>
+    </form>
 
-<ul>
-    {#each data.todos as todo}
-        <li>
-            <span>{todo.title}</span>
-        </li>
-    {/each}
-</ul>
+    <ul class="flex flex-col justify-center gap-8">
+        {#each data.todos as todo}
+            <li>
+                <span>{todo.title}</span>
+            </li>
+        {/each}
+    </ul>
+</article>
