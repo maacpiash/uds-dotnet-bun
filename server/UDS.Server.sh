abb51fa6da9e5d15cb38b487e8f7973dbccacbda
Ahad Chowdhury <ahad@maacpiash.com>
2024-06-18 00:07:19 +1000

    🔨 Add command for creating todos with POST request

--- server/UDS.Server.sh
+++ server/UDS.Server.sh
@@ -1,1 +1,10 @@
+# POST
+curl -w "\n" \
+    --header "Content-Type: application/json" \
+    --request POST \
+    --data '{ "title": "Wash the dishes" }' \
+    --unix-socket /tmp/uds-dotnet-bun.sock \
+    http://localhost/todos
+
+# GET
 curl -w "\n" --unix-socket /tmp/uds-dotnet-bun.sock http://localhost/todos | jq
